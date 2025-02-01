e0771de161f66e5df2669cd0be2ba0a1e4368a92
Rajkan31 <thirunamam.rajesh@gmail.com>
2025-02-01 15:18:39 +0530

    new code added

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,32 @@
+package org.emp;
+
+public class Employee {
+	private void empId() {
+		System.out.println("100");
+	}
+	private void empName() {
+		System.out.println("Rajesh Kannan");
+	}
+	private void empDob() {
+	    System.out.println("31-10-1993");
+	}
+	private void empPhone() {
+		System.out.println("8124032050");
+	}
+	private void empEmail() {
+		System.out.println("xyz@infor.com");
+	}
+	private void empAddress() {
+		System.out.println("Hyderabad");
+	}
+	public static void main(String[] args) {
+		Employee obj=new Employee();
+		obj.empId();
+		obj.empName();
+		obj.empDob();
+		obj.empPhone();
+		obj.empEmail();
+		obj.empAddress();
+	}
+}
+
