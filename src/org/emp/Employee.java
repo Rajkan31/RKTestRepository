004352067687649b7849a0231dfba6bd937bf2ed
Rajkan31 <thirunamam.rajesh@gmail.com>
2025-02-09 11:55:08 +0530

    new code added by narmada

--- src/org/emp/Employee.java
+++ src/org/emp/Employee.java
@@ -2,10 +2,10 @@ package org.emp;
 
 public class Employee {
 	private void empId() {
-		System.out.println("100");
+		System.out.println("10");
 	}
 	private void empName() {
-		System.out.println("Rajesh Kannan");
+		System.out.println("Rajesh Kannan T");
 	}
 	private void empDob() {
 	    System.out.println("31-10-1993");
@@ -14,11 +14,14 @@ public class Employee {
 		System.out.println("8124032050");
 	}
 	private void empEmail() {
-		System.out.println("ABC@infor.com");
+		System.out.println("Haha@infor.com");
+		System.out.println("Changes done by Narmada");
 	}
 	private void empAddress() {
 		System.out.println("Hyderabad");
 	}
+	
+	
 	public static void main(String[] args) {
 		Employee obj=new Employee();
 		obj.empId();
